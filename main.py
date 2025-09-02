70a935f8f4d66aa407a29ec59b0569e40499cf0a
1846475884@qq.com <1846475884@qq.com>
2025-09-02 19:36:26 +0800

    update lama

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def main():
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    # import tarfile
-    # with tarfile.open("IORT.tar.gz", "w:gz") as tar:
-    #     tar.add("IORT", arcname="IORT")
-    #     tar.add("LaMaProject", arcname="LaMaProject")
-    #     tar.add("main.py")
+    import tarfile
+    with tarfile.open("IORT.tar.gz", "w:gz") as tar:
+        tar.add("IORT", arcname="IORT")
+        tar.add("LaMaProject", arcname="LaMaProject")
+        tar.add("main.py")
     main()
 
     
